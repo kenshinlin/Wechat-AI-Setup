f9989b3391159720f8c6a9e5e73c7752a679421a
kenshinlin <kenniu.top@gmail.com>
2024-06-20 10:44:50 +0800

    chore: set prefix

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
 const withNextra = require('nextra')({
   theme: 'nextra-theme-docs',
   themeConfig: './theme.config.tsx',
+  basePath: '/doc',
+  assetPrefix: '/doc',
 })
 
 module.exports = withNextra()
