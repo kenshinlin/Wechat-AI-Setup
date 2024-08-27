980d6af32533e9b8c6ed32296551412e839b713a
kenshinlin <kenniu.top@gmail.com>
2024-08-27 10:08:25 +0800

    doc: update doc

--- pages/layout.tsx
+++
@@ -1,8 +0,0 @@
-import type { Metadata } from "next";
-
-export const metadata: Metadata = {
-  title: " 微信 AI 连接器 | 慢壳 AI - 使用文档",
-  keywords: ['Wechat', 'ChatGPT 微信', 'Kimi', '月之暗面,' 'coze', '扣子', 'AI，智能体', 'AI 机器人'],
-  description:
-    "连接微信 和 AI，支持国内几乎所有类似 ChatGPT 的大模型，如 Kimi，豆包，义通千问、GLM 等，实现微信聊天和 AI 对话的连接。对接 Coze 智能体，自定义插件、工作流和图片流，使您的 AI 机器人无所不能",
-};
--- theme.config.tsx
+++ theme.config.tsx
@@ -13,6 +13,32 @@ const config: DocsThemeConfig = {
   footer: {
     text: "慢壳 AI 专注连接微信和 AI",
   },
+  useNextSeoProps() {
+    return {
+      additionalLinkTags: [
+        {
+          rel: "icon",
+          href: "https://www.weai.chat/logo.ico",
+        },
+      ],
+      keywords:
+        "Wechat AI, ChatGPT, 微信 AI, Kimi, 月之暗面, coze, 扣子, 智能体, AI 机器人",
+      titleTemplate: "%s - 慢壳 AI",
+      description:
+        "连接微信 和 AI，支持国内几乎所有类似 ChatGPT 的大模型，如 Kimi，豆包，义通千问、GLM 等，实现微信聊天和 AI 对话的连接。对接 Coze 智能体，自定义插件、工作流和图片流，使您的 AI 机器人无所不能",
+      openGraph: {
+        type: "website",
+        locale: "zh_CN",
+        url: "https://www.weai.chat",
+        site_name: "慢壳 AI",
+        images: "https://www.weai.chat/logo.png",
+      },
+      twitter: {
+        cardType: "summary_large_image",
+        site: "https://www.weai.chat",
+      },
+    };
+  },
 };
 
 export default config;
