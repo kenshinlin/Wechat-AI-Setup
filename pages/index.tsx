819df5be00e2f4f3af8a418815fd000bcddf1924
kenshinlin <kenniu.top@gmail.com>
2025-02-21 21:14:32 +0800

    feat: update

--- pages/index.tsx
+++ pages/index.tsx
@@ -358,7 +358,7 @@ export default function Home() {
       </div>
 
       <div className="bg-gray-100 py-8">
-        <a href="" name="pricing"></a>
+        <a href="" id="pricing"></a>
         <div className="container mx-auto px-2 pt-4 pb-12 text-gray-800">
           <h2 className="w-full my-2 text-5xl font-bold leading-tight text-center text-gray-800">
             免费入门使用
